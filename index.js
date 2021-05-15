380e734dfb7178f69cd7342861678d8130c23635
Wisang Jati Anggoro <wisang1999@gmail.com>
2021-05-15 23:40:26 +0700

    adding ||process.env.PORT to const PORT

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = 3001||process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
